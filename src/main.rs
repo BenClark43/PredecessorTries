ef7bbc48f804a7eeb0789d45b4ecc17be6a47077
BenClark43 <BenClark43@Outlook.com>
2025-03-28 10:02:06 -0500

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,73 @@
 mod xfast_trie;
 
-// Declare the module from the `person.rs` file
-use xfast_trie::XFastTrie; // Bring the `Person` struct into scope
+use csv::Writer;
+use rand::Rng;
+use std::collections::HashSet;
+use std::error::Error;
+use std::path::Path;
+use std::time::{Duration, Instant};
+use xfast_trie::XFastTrie;
+
+fn create_csv_file(file_path: &str, headers: &[&str]) -> Result<(), Box<dyn Error>> {
+    if !Path::new(file_path).exists() {
+        let mut wtr = Writer::from_path(file_path)?;
+        wtr.write_record(headers)?;
+        wtr.flush()?;
+    }
+    Ok(())
+}
+
+fn save_results() -> Result<(), Box<dyn Error>> {
+    create_csv_file(
+        "results.csv",
+        &[
+            "Data Structure",
+            "Universe Size",
+            "# of Values",
+            "Time",
+            "Memory",
+        ],
+    );
+    let mut wtr = Writer::from_path("results.csv")?;
+    wtr.write_record(&["X-Fast Trie", "2^8", "2^7", "time", "memory"])?;
+    wtr.flush()?;
+    Ok(())
+}
 
 fn main() {
+    //let mut rng = rand::thread_rng();
+    //let mut random_values = HashSet::new();
+
+    //while random_values.len() < 85000000 {
+    //    random_values.insert(rng.gen::<u32>());
+    // }
+
+    //let start = Instant::now();
+    //let mut duration = start.elapsed();
 
     let mut test: XFastTrie<u32> = XFastTrie::new();
-    let _ = test.insert(15, 1000);
-    _ = test.insert(16, 2000);
-    _ = test.insert(27, 3000);
-    _ = test.insert(33, 5000);
-   test.hashmap_contents();
-   
-    println!("15 = {}, 16 = {}, 27 = {}", test.get(&15).unwrap(), test.get(&16).unwrap(), test.get(&27).unwrap());
-    println!("{}",test.predecessor(&18).unwrap());
-    println!("{}",test.predecessor(&19).unwrap());
-    println!("{}",test.predecessor(&27).unwrap());
-    println!("{}",test.predecessor(&16).unwrap());
-    println!("{}",test.predecessor(&26).unwrap());
-    println!("{}",test.predecessor(&35).unwrap());
+    // test.insert(200, 200);
 
-    
-/*,
-    let mut level = 1;
-    let max_levels = 9;
-    while level < max_levels {
-        let prefix = 179 >> (max_levels - level);
-        println!("prefix: {:8b}", prefix);
-        level += 1;
+    for value in 1..4 {
+        test.insert(value, value);
     }
-*/
 
+    //for value in &random_values {
+    //    test.insert(*value, *value);
+    //}
+
+    test.hashmap_contents();
+    /*
+
+       duration = start.elapsed();
+       println!("Time taken: {:?}", duration);
+       for value in (1..65535) {
+           test.predecessor(&value);
+       }
+
+       duration = start.elapsed();
+       println!("Time taken: {:?}", duration);
+       save_results();
+
+    */
 }
--- src/xfast_trie.rs
+++ src/xfast_trie.rs
@@ -2,15 +2,15 @@ use std::cell::RefCell;
 use std::collections::HashMap;
 use std::rc::Rc;
 
-const MAX_KEY_SIZE: usize = 6;
-const MAX_LEVELS: i32 = 6;
+const MAX_KEY_SIZE: usize = 31;
+const MAX_LEVELS: i32 = 31;
 
 // We use Rc<RefCell<>> because nodes are mutable and stored both in the tree and in the hashmaps
 
 #[derive(Debug)]
 struct XFastNode<V>
 where
-    V: Clone
+    V: Clone,
 {
     key: Option<u32>,
     value: Option<V>,
@@ -20,7 +20,7 @@ where
 
 impl<V> XFastNode<V>
 where
-    V: Clone
+    V: Clone,
 {
     pub fn new() -> Self {
         XFastNode {
@@ -48,7 +48,6 @@ where
         self.right = Some(node);
     }
 
-    
     pub fn get_left(&self) -> Option<Rc<RefCell<XFastNode<V>>>> {
         self.left.as_ref().map(|node| Rc::clone(node))
     }
@@ -56,16 +55,17 @@ where
     pub fn get_right(&self) -> Option<Rc<RefCell<XFastNode<V>>>> {
         self.right.as_ref().map(|node| Rc::clone(node))
     }
-    
 
+    pub fn is_leaf(&self) -> bool {
+        self.value.is_some() && self.key.is_some()
+    }
 }
 
 #[derive(Debug)]
 pub struct XFastTrie<V>
 where
-    V: Clone
+    V: Clone,
 {
-   
     max_leaf: Option<Rc<RefCell<XFastNode<V>>>>,
     min_leaf: Option<Rc<RefCell<XFastNode<V>>>>,
     root: XFastNode<V>,
@@ -74,17 +74,21 @@ where
 
 impl<V> XFastTrie<V>
 where
-    V: Clone + std::fmt::Display
+    V: Clone + std::fmt::Display,
 {
     pub fn new() -> Self {
-
         let root = XFastNode::new();
         let max_leaf = None;
-        let min_leaf =  None;
+        let min_leaf = None;
         let hashmaps: Vec<HashMap<u32, Rc<RefCell<XFastNode<V>>>>> =
             vec![HashMap::new(); MAX_KEY_SIZE];
 
-        XFastTrie { max_leaf, min_leaf, root, hashmaps }
+        XFastTrie {
+            max_leaf,
+            min_leaf,
+            root,
+            hashmaps,
+        }
     }
 
     pub fn insert(&mut self, key: u32, value: V) -> bool {
@@ -93,214 +97,189 @@ where
         }
 
         let leaf_pointer: Rc<RefCell<XFastNode<V>>> =
-            Rc::new(RefCell::new(XFastNode::new_leaf(key,value)));
+            Rc::new(RefCell::new(XFastNode::new_leaf(key, value)));
 
-        // Once these operations are working we can link the leaf here
-           // let pred: Option<Rc<RefCell<XFastNode<V>>>>;
-            match self.predecessor(&key){
-                Some(pred) =>{ leaf_pointer.borrow_mut().set_left(Rc::clone( self.hashmaps[MAX_KEY_SIZE-1]
-                    .get(&pred)
-                    .unwrap()));
-                },
-                None => {}
-            };
-            // succ = self.successor(key)
-            // leaf_pointer.set_right(Rc::clone(&succ));
+        match self.predecessor(&key) {
+            Some(pred) => {
+                leaf_pointer.borrow_mut().set_left(Rc::clone(
+                    self.hashmaps[MAX_KEY_SIZE - 1].get(&pred).unwrap(),
+                ));
+            }
+            None => {}
+        };
 
+        //Fix internal nodes pointers
         let mut curr_size: usize = self.longest_prefix_search(&key);
+        for prefix_length in (1..curr_size) {
+            let temp = (key >> (MAX_KEY_SIZE - prefix_length));
+            let node_ref = self.hashmaps[prefix_length].get(&(key >> (MAX_KEY_SIZE - prefix_length))).unwrap().clone();
+            let mut node = node_ref.borrow_mut();
+            let left_ref = node.get_left().unwrap();
+            let right_ref = node.get_right().unwrap();
+
+
+            {
+                let left = Rc::clone(left_ref);
+                if left.is_leaf() && key < left.key.as_ref().unwrap().clone() {
+                    drop(left);
+                    node.set_left(Rc::clone(&leaf_pointer));
+                }
+            }
+            /*
+            {
+                let left = left_ref.borrow();
+                if left.is_leaf() && key < left.key.as_ref().unwrap().clone() {
+                    drop(left);
+                    node.set_left(Rc::clone(&leaf_pointer));
+                }
+            }
+            {
+                let right = right_ref.borrow();
+                if right.is_leaf() && key > right.key.as_ref().unwrap().clone() {
+                    drop(right);
+                    node.set_right(Rc::clone(&leaf_pointer));
+                }
+            }
+            drop(node);
+
+             */
+        }
 
         // Find the first node
         let mut curr_node: Rc<RefCell<XFastNode<V>>> = match curr_size {
             0 => {
                 let first_node: Rc<RefCell<XFastNode<V>>> = Rc::new(RefCell::new(XFastNode::new()));
-                self.hashmaps[0].insert((key>> (MAX_KEY_SIZE-1)) & 1, Rc::clone(&first_node));
+                self.hashmaps[0].insert((key >> (MAX_KEY_SIZE - 1)) & 1, Rc::clone(&first_node));
                 if key & 1 > 0 {
                     self.root.set_right(Rc::clone(&first_node));
                 } else {
                     self.root.set_left(Rc::clone(&first_node));
                 }
-                if(self.max_leaf.is_none()){
+                if (self.max_leaf.is_none()) {
                     self.max_leaf = Some(leaf_pointer.clone());
                 }
-                if self.min_leaf.is_none(){
+                if self.min_leaf.is_none() {
                     self.min_leaf = Some(leaf_pointer.clone());
                 }
-            
+
                 curr_size = 1;
                 first_node
             }
-            _ => { 
+            _ => {
                 let bmax_leaf = self.max_leaf.as_ref();
                 let max_leaf_key = bmax_leaf.unwrap().borrow().key;
                 let bmin_leaf = self.min_leaf.as_ref();
                 let min_leaf_key = bmin_leaf.unwrap().borrow().key;
-                if(max_leaf_key.unwrap() < key){
+                if (max_leaf_key.unwrap() < key) {
                     self.max_leaf = Some(Rc::clone(&leaf_pointer));
                 }
-                if(min_leaf_key.unwrap() > key){
+                if (min_leaf_key.unwrap() > key) {
                     self.min_leaf = Some(Rc::clone(&leaf_pointer));
                 }
-            
+
                 Rc::clone(
-                    self.hashmaps[curr_size-1]
+                    self.hashmaps[curr_size - 1]
                         .get(&(key >> (MAX_KEY_SIZE - curr_size)))
-                        .unwrap()
+                        .unwrap(),
                 )
-            },
+            }
         };
 
         // Add the internal Nodes
         while curr_size < MAX_KEY_SIZE - 1 {
             curr_size += 1;
-            let mut next_node: Rc<RefCell<XFastNode<V>>> =
-                Rc::new(RefCell::new(XFastNode::new()));
+            let mut next_node: Rc<RefCell<XFastNode<V>>> = Rc::new(RefCell::new(XFastNode::new()));
 
             let mut curr_node_ref = curr_node.borrow_mut();
-            if curr_size == MAX_KEY_SIZE - 1{
+            if curr_size == MAX_KEY_SIZE - 1 {
                 next_node = leaf_pointer.clone();
             }
             if (key & (1 << (MAX_KEY_SIZE - curr_size))) != 0 {
-                if curr_node_ref.get_left().is_some(){
+                if curr_node_ref.get_left().is_some() {
                     curr_node_ref.set_right(Rc::clone(&next_node));
-                }
-                else{
+                } else {
                     curr_node_ref.set_right(Rc::clone(&next_node));
                     curr_node_ref.set_left(Rc::clone(&leaf_pointer));
                 }
-                
             } else {
                 if curr_node_ref.get_right().is_some() {
                     curr_node_ref.set_left(Rc::clone(&next_node));
-                }
-                else{
+                } else {
                     curr_node_ref.set_right(Rc::clone(&leaf_pointer));
                     curr_node_ref.set_left(Rc::clone(&next_node));
                 }
             }
             drop(curr_node_ref);
-            self.hashmaps[curr_size-1].insert(key >> (MAX_KEY_SIZE - curr_size), Rc::clone(&next_node));
+            self.hashmaps[curr_size - 1]
+                .insert(key >> (MAX_KEY_SIZE - curr_size), Rc::clone(&next_node));
             curr_node = next_node;
         }
 
-        //curr_node_ref.set_right(Rc::clone(&leaf_pointer));
-    
+        let mut curr_node_ref = curr_node.borrow_mut();
+        if curr_node_ref.get_right().is_some() {
+            curr_node_ref.set_left(Rc::clone(&leaf_pointer));
+        } else {
+            curr_node_ref.set_right(Rc::clone(&leaf_pointer));
+            curr_node_ref.set_left(Rc::clone(&leaf_pointer));
+        }
+
         self.hashmaps[curr_size].insert(key, Rc::clone(&leaf_pointer));
         true
     }
 
     pub fn get(&self, key: &u32) -> Option<V> {
-        self.hashmaps[MAX_KEY_SIZE-1]
+        self.hashmaps[MAX_KEY_SIZE - 1]
             .get(key)
             .and_then(|node| node.borrow().value.clone())
     }
 
     pub fn contains(&self, key: &u32) -> bool {
-        self.hashmaps[MAX_KEY_SIZE-1]
-            .contains_key(key)
+        self.hashmaps[MAX_KEY_SIZE - 1].contains_key(key)
     }
 
     // WIP
     pub fn predecessor(&mut self, key: &u32) -> Option<u32> {
         let mut largest_prefix: usize = self.longest_prefix_search(&key);
         let curr_node: Rc<RefCell<XFastNode<V>>>;
-        if(largest_prefix == 0){
-            if(self.root.get_left().is_none() && self.root.get_right().is_none()){
+        if (largest_prefix == 0) {
+            if (self.root.get_left().is_none() && self.root.get_right().is_none()) {
                 return None;
-            }
-            else if(self.root.get_left().is_some()){
+            } else if (self.root.get_left().is_some()) {
                 return self.max_leaf.as_ref().unwrap().borrow().key;
-            }
-            else{
+            } else {
                 return self.min_leaf.as_ref().unwrap().borrow().key;
             }
-        }
-        else{
-         curr_node = self.hashmaps[largest_prefix-1]
-            .get(&(key >> (MAX_KEY_SIZE - largest_prefix)))
-            .unwrap().clone();
+        } else {
+            curr_node = self.hashmaps[largest_prefix - 1]
+                .get(&(key >> (MAX_KEY_SIZE - largest_prefix)))
+                .unwrap()
+                .clone();
         }
         if largest_prefix == MAX_KEY_SIZE {
             return curr_node.borrow().key;
-        }
-        else{
-            let pred_node : Rc<RefCell<XFastNode<V>>>;
+        } else {
+            let pred_node: Rc<RefCell<XFastNode<V>>>;
             //check which child node we have
-            match self.hashmaps[largest_prefix].get(&((key >> (MAX_KEY_SIZE - largest_prefix)) << 1)) {
+            match self.hashmaps[largest_prefix]
+                .get(&((key >> (MAX_KEY_SIZE - largest_prefix)) << 1))
+            {
                 //node has left child
                 Some(_) => {
-                     pred_node = curr_node.borrow().get_right().unwrap();
-                },
+                    pred_node = curr_node.borrow().get_right().unwrap();
+                }
                 //node has right child
                 None => {
                     pred_node = curr_node.borrow().get_left().unwrap();
-                },
+                }
             };
             if pred_node.borrow().key.as_ref().unwrap() < key {
                 return pred_node.borrow().key;
-            }
-            else {
+            } else {
                 let next_node = pred_node.borrow().get_left().unwrap();
                 return next_node.borrow().key;
             }
         }
 
-
-        /* here is a normal bitwise trie predecessor algorithm lol, gonna leave it here for fun 
-        else {
-            let mut size = largest_prefix;
-            match curr_node.borrow().get_left()  {
-
-                //prefix node has left child
-                Some(mut node) => {
-                    size += 1; 
-                    while(size < MAX_KEY_SIZE){
-                        match node.borrow().get_right() {
-                            Some(next_node) => {
-                                node = next_node;
-                                size += 1;
-                            },
-                            None => {
-                                node = node.borrow().get_left().unwrap();
-                                size += 1;
-                            }
-                        }
-                    }
-                    node.borrow().value.clone()
-                },
-
-                //prefix node has no left child
-                None => {
-                    //go back up the tree until you get the parent of the rightmost 1 bit
-                    //set node equal to its left child then take the largest key in that subtree
-                    let mut prefix_key = key >> (MAX_KEY_SIZE - largest_prefix);
-                    size = MAX_KEY_SIZE - ((prefix_key.trailing_zeros() as usize) + 2);
-                    curr_node = self.hashmaps[size-1].get(&(key >> (key.trailing_zeros() as usize + 2))).unwrap();
-                    //largest key from this 
-                    while(size < MAX_KEY_SIZE){
-                        match curr_node.borrow().get_right() {
-                            Some(next_node) => {
-                                curr_node = next_node;
-                                size += 1;
-                            },
-                            None => {
-                                curr_node = curr_node.borrow().get_left().as_ref().unwrap();
-                                size += 1;
-                            }
-                        } 
-                    }
-                    curr_node.borrow().value.clone()
-                },
-            }
-        };
-         */
-        
-        //println!("prefix key: {}", curr_node.borrow().value.clone().unwrap());
-
-       // let left_node: Rc<RefCell<XFastNode<V>>> =
-        //     Rc::new(RefCell::new(XFastNode::new(key & ((1 << curr_size) - 1))));
-
-      //  println!("Left key: {}", curr_node.borrow().left.clone().unwrap().value);
-      //println!("Right key: {}", curr_node.borrow().right.clone().unwrap());
     }
 
     // For Debugging
@@ -319,18 +298,17 @@ where
         let mut high = MAX_KEY_SIZE;
         let mut largest_prefix: usize = 0;
 
-        while low <= high && high > 0{
+        while low <= high && high > 0 {
             let prefix_size = low + (high - low) / 2;
 
-            if self.hashmaps[prefix_size-1].contains_key(&(key >> (MAX_KEY_SIZE - prefix_size))) {
+            if self.hashmaps[prefix_size - 1].contains_key(&(key >> (MAX_KEY_SIZE - prefix_size))) {
                 largest_prefix = prefix_size;
                 low = prefix_size + 1;
             } else {
-                high = prefix_size -1;
+                high = prefix_size - 1;
             }
         }
 
         largest_prefix
     }
-
 }
